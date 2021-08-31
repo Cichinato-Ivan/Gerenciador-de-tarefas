8bda262d0042885eafffe3791fe7775f102fafe4
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2021-08-31 23:24:08 +0100

    validando forms

--- js/Index.js
+++ js/Index.js
@@ -1,11 +1,9 @@
-var name = document.querySelector("#inputName");
-var price = document.querySelector("#inputPrice");
-var portion = document.querySelector("#inputPortion");
-var description = document.querySelector("#inputDescription");
-
-
 var fields = document.querySelectorAll("#form-accounts-create [name]");
 
+var user = {};
+
 fields.forEach(function(field, index) {
-    console.log(field);
+
+    user[field.name] = field.value;
+
 });
