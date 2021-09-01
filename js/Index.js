d682bbed819f3179a6f425d3cc666439b75d2956
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2021-09-01 21:58:04 +0100

    dinamica em processo

--- js/Index.js
+++ js/Index.js
@@ -1,6 +1,33 @@
 var fields = document.querySelectorAll("#form-accounts-create [name]");
-
 var user = {};
+
+function addLine(dataAccounts) {
+  
+    var ul = document.getElement("ul");
+  
+  ul.innerHTML = `
+    
+    <ul class="list-group-item rounded-3">
+        <li>
+            <div class="row mt-2">
+                <div class="col-md-10">
+                        <p>${dataAccounts.name}</p>
+                        <p>${dataAccounts.price}</p>
+                        <p>${dataAccounts.portion}</p>
+                        <p>${dataAccounts.description}</p>
+                </div>
+                <div class="col-md-2">
+                    <a href="">
+                        <img src="./icons/info-square.svg" alt="" width="25px" height="25px">
+                    </a>
+                </div>
+            </div>
+        </li>
+    </ul>
+  `;
+
+  document.getElementsById("list-accounts").appendChild("ul");
+}
 
 document.getElementById("form-accounts-create").addEventListener("submit", function(event){
 
@@ -12,6 +39,6 @@ document.getElementById("form-accounts-create").addEventListener("submit", funct
     
     });
     
-    console.log(user);
+    addLine(user);
 
 });
